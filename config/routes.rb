fc90e628a59f4c824f85651cfd129e808e56f633
Temiyemi Agbetunsin <temi.yemi@gmail.com>
2012-04-03 12:54:05 +0100

    Switched to Admin branch; integrated Rails Admin models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Ghtrains::Application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+
+  devise_for :staffs
+
+  root to: 'index#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
