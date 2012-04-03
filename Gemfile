01d306e8044303d95bfa730770fb2eadd0f92968
Temiyemi Agbetunsin <temi.yemi@gmail.com>
2012-04-03 12:38:48 +0100

    Initialized new Project for GhTrains server-side application

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.2'
+gem 'sqlite3'
+gem 'devise', '2.0.4'
+gem 'rails_admin', :git => 'git://github.com/sferik/rails_admin.git'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ghtrains::Application.config.secret_token = 'dd47dcc22553e596dac3e1335e394315bff64de251e72a7766ac37735e351708e093a91cbd1ee034b57b1437c06a520e1d72e941f0d20649bd649a60ba999789'
